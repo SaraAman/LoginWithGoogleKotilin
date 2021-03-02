22da6d020fbee70813e442278a536867027591a9
SaraAman <79627873+SaraAman@users.noreply.github.com>
2021-03-02 12:39:57 +0500

    LoginWithGoogleKotilin

---
+++ app/src/main/java/com/example/googleloginkotlin/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.googleloginkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val RC_SIGN_IN = 123
+
+class MainActivity : AppCompatActivity() {
+
+    //val profiletext: TextView = findViewById(R.id.sign_text)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        //val sign_in_button = findViewById(R.id.sign_in_button) as SignInButton
+
+
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+
+            sign_text.text = acct.displayName
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+        if (requestCode == RC_SIGN_IN) {
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+
+        try {
+
+            val account = completedTask.getResult(ApiException::class.java)
+            sign_text.text = account.toString()
+        } catch (e: ApiException) {
+            sign_text.text = ""
+        }
+    }
+
+
+}
